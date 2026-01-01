af83beefa80c587b3f0f8d64d262d63fa034299a
DaniPooh777 <danipooh777@gmail.com>
2026-01-01 18:08:37 +0100

    .

--- Web_ACMA/components/link_button.py
+++ Web_ACMA/components/link_button.py
@@ -4,7 +4,7 @@ from Web_ACMA.styles.components_style.navbar_style import HERO_BUTTON_STYLE
 def link_button() -> rx.Component:
     return rx.link(
         rx.button(
-            "Solicite un nuevo encargo",
+            "Nuevo encargo",
             style=HERO_BUTTON_STYLE # <--- PONETE LAS PILAS ACÁ
         ),
         href="/contact#formulario-encargo",
