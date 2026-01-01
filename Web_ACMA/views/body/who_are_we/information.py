aef82332f839623fe0eadd26ab72bcf6b5a1f5b5
DaniPooh777 <danipooh777@gmail.com>
2026-01-01 18:40:38 +0100

    Parte texto Quienes Somos

--- Web_ACMA/views/body/who_are_we/information.py
+++ Web_ACMA/views/body/who_are_we/information.py
@@ -7,19 +7,31 @@ def information() -> rx.Component:
         # Tarjeta superior (Quiénes Somos)
         information_card(
             "Quiénes Somos",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            """ACMA (Agencia de Contenido de Manyanet Alcobendas) es un equipo creativo de alumnos de 2º de Bachillerato 
+            de Ciencias de la Computación, encargado de crear contenido digital para el centro. Su misión es conceptualizar, 
+            diseñar y desarrollar aplicaciones, material didáctico y de divulgación, especializándose en presentaciones 
+            interactivas y asegurando la calidad en la documentación (técnica y académica) y el diseño gráfico. ACMA 
+            funciona como un laboratorio para aplicar conocimientos, fomentar el pensamiento crítico, el trabajo en 
+            equipo y el dominio de herramientas de desarrollo.""",
             width="100%"
         ),
-        # Contenedor para Misión y Visión
+        # Contenedor inferior para Misión y Visión
         rx.flex(
             information_card(
                 "Misión",
-                "Lorem Ipsum dolor sit amet, consectetur adipiscing elit. Ut enim ad minim veniam, quis nostrud exercitation.",
-                width=["100%", "48%"] # Casi la mitad para que el gap no las rompa
+                """Nuestra misión es simplificar la vida de los educadores, permitiéndoles centrarse en la conexión con 
+                los estudiantes y la preparación pedagógica, y no en la carga administrativa. Aligeramos drásticamente 
+                el trabajo, proporcionando herramientas y contenido para crear material didáctico digital de forma rápida 
+                y de calidad. Así, los docentes invierten su tiempo en lo verdaderamente importante: la planificación, la 
+                personalización, la interacción y la mentoría.""",
+                width=["100%", "48%"]
             ),
             information_card(
                 "Visión",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                """ACMA (Agencia de Contenido Manyanet Alcobendas) es un referente interno en excelencia digital. Alumnos de 
+                2º de Bachillerato de Ciencias de la Computación crean soluciones digitales (apps, material didáctico, documentación) 
+                que simplifican la labor pedagógica y liberan a los profesores de carga técnica. Fomenta talento digital, pensamiento 
+                crítico y colaboración, impulsando la innovación educativa.""",
                 width=["100%", "48%"]
             ),
             width="100%",
