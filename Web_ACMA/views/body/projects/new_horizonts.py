0bbbcd0607e9f1f59592e1f76d4604da1d756584
DaniPooh777 <danipooh777@gmail.com>
2026-03-20 23:48:59 +0100

    Arreglo texto y corrección tamaño de la imagen para que sea parejo en Nuevos Horizontes

--- Web_ACMA/views/body/projects/new_horizonts.py
+++ Web_ACMA/views/body/projects/new_horizonts.py
@@ -21,10 +21,15 @@ def new_horizonts() -> rx.Component:
             rx.vstack(
                 rx.heading("Propuesta de Innovación", style=INFO_TITLE_STYLE),
                 rx.text(
-                    """A partir de este año, en ACMA, estamos abiertos a la idea de expandirnos en el área de la
-                    programación. Nuestro objetivo es crear proyectos personalizados para tareas complejas, 
-                    como esta misma página web, desarrollada por Daniel González para que ACMA sea accesible 
-                    para todos los niveles educativos.""",
+                    """A partir de este año, en ACMA, estamos abiertos a la idea de de expandirnos en el área de la
+                    programación. Nuesto objetivo detrás de ello es de crear la posibilidad de poder desarrollar
+                    proyectos más personalizados para aquellas tareas más complejas que crear una presentación
+                    interactiva en Genially. Un claro ejemplo de ello es esta página web. Fue creada por el 
+                    colaborador principal de ACMA Daniel González, cuya visión es que ACMA sea más accesible y
+                    entendible para todos los profesores independientemente de si es de Guardería o de Bachillerato.
+                    Por esta razón, consideramos la importancia de intentar establecer en ACMA la programación como
+                    uno de nuestros servicios fijos para así poder mejorar la enseñanza con proyectos personalizados
+                    para las necesidades de cada profesor.""",
                     style=INFO_TEXT_STYLE,
                     text_align="justify"
                 ),
@@ -43,7 +48,7 @@ def new_horizonts() -> rx.Component:
                 style={
                     **FOUNDER_IMAGE_STYLE, 
                     "width": ["100%", "450px"], 
-                    "height": "auto", 
+                    "height": ["100%", "450px"], 
                     "border_radius": "15px",
                     "margin_top": ["1.5rem", "1.5rem", "0rem"],
                     "box_shadow": "0px 4px 20px rgba(0,0,0,0.2)" 
@@ -55,7 +60,7 @@ def new_horizonts() -> rx.Component:
                 **FOUNDER_SECTION_CONTAINER, 
                 "max_width": "1150px", 
                 "width": "100%",
-                "padding_x": ["1.5rem", "2rem"],
+                "padding_x": ["1rem", "1rem"],
                 "gap": ["2rem", "4rem"], 
             },
             flex_direction=["column", "column", "row"],
