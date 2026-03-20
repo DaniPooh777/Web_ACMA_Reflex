2c43950e5bcdb5d0f4f2aebf230cd38619a38337
DaniPooh777 <danipooh777@gmail.com>
2026-03-20 22:31:19 +0100

    Responsiva sección Nuestro Fundador.

--- Web_ACMA/views/body/who_are_we/founder.py
+++ Web_ACMA/views/body/who_are_we/founder.py
@@ -14,7 +14,13 @@ def founder() -> rx.Component:
     return rx.center(
         rx.vstack(
             # Título de arriba
-            rx.heading("Nuestro Fundador", style=FOUNDER_SECTION_TITLE_STYLE),
+            rx.heading("Nuestro Fundador", 
+                style={
+                    **FOUNDER_SECTION_TITLE_STYLE,
+                    "font_size": ["2rem", "2.7rem"], 
+                    "padding_x": "1rem"
+                    }
+                ),
             
             # Contenedor de imagen + tarjeta
             rx.flex(
@@ -22,15 +28,16 @@ def founder() -> rx.Component:
                 rx.image(
                     src="WebMaster.jpg",
                     alt="Imagen del fundador",
-                    style=FOUNDER_IMAGE_STYLE
+                    style={
+                        **FOUNDER_IMAGE_STYLE,
+                        "width": ["200px", "350px"], 
+                        "height": ["200px", "350px"],
+                    }
                 ),
                 # Tarjeta a la derecha
                 rx.vstack(
                     rx.heading("José Antonio Romero Paniagua", style=INFO_TITLE_STYLE),
-                    rx.text(
-                        "Fundador y Director de ACMA",
-                        style=INFO_SUBTITTLE_STYLE
-                    ),
+                    rx.text("Fundador y Director de ACMA", style=INFO_SUBTITTLE_STYLE),
                     rx.text(
                         """ACMA nació bajo el respaldo de Jefatura de Estudios como un proyecto innovador dentro de la optativa Ciencias de la Computación. 
                         Nuestra misión es doble: facilitar la labor docente con materiales de alta calidad y ofrecer al alumno una experiencia laboral real 
@@ -38,11 +45,21 @@ def founder() -> rx.Component:
                         style=INFO_TEXT_STYLE,
                         text_align="justify"
                     ),
-                    style=FOUNDER_TEXT_CARD_STYLE,
+                    style={
+                        **FOUNDER_TEXT_CARD_STYLE,
+                        "width": "100%", 
+                        "padding": ["1.5rem", "3rem"], 
+                    },
                     align_items="start",
                     justify_content="center",
                 ),
-                style=FOUNDER_SECTION_CONTAINER,
+                style={
+                    **FOUNDER_SECTION_CONTAINER,
+                    "gap": ["1.5rem", "3rem"], 
+                    "padding_x": ["1rem", "2rem"],
+                },
+                flex_direction=["column", "row"], 
+                align_items="center",
             ),
             width="100%",
             max_width="1100px",
